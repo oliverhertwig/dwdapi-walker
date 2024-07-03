093e5e3b74535d2be4e2c81b6066fc1562a68216
Oliver Hertwig <ohertwig@gmail.com>
2024-07-03 06:50:13 +0200

    Change weather.py to work with brightsky API, add temprz as code example for distribution, alter exmpl_shrt.json to get a temperature reading

--- weather.py
+++ weather.py
@@ -1,28 +1,30 @@
 #!/usr/bin/env python3
-# import urllib3
-# from urllib.request import urlopen 
+#import urllib3
+from urllib.request import urlopen 
 import json
 # http = urllib3.PoolManager()
 # site = http.request("get", "https://api.brightsky.dev/weather?dwd_station_id=06313&date=2024-07-01").data
 # url = "https://api.brightsky.dev/weather?dwd_station_id=06313&date=2024-07-01"
-# response = urlopen(url) 
-# data = json.loads(response.read()) 
-# data = json.loads(site)
+# 06313 Breckerfeld Wengeberg 03098 Luedenscheid 01766 Muenster/Osnabrueck
+url = "https://api.brightsky.dev/current_weather?dwd_station_id=01766&date=2024-07-01"
+response = urlopen(url) 
+data = json.loads(response.read()) 
 # print(data)
-with open('dreck.json','r') as f:
-    data = json.load(f)
-    print(type(data))
-    print(data.keys())
-    print(data.values())
-    # oli = data.get("id")
-    # print(oli)
-    # test = data['weather']
-    # getting hold of the values of 
-    # variableParam
-    # test1 = test[0].values()  
-    # list(test.values())
-    # print(test1)
-    # test2 = list(test1)[0]
-    # test3 = test2[1:-1].split(",")
-    # print(test3[1])
-    print("mee")
+# for testimg purposes use downloaded json file
+# with open('exmpl_shrt.json','r') as f:
+    #data = json.load(f)
+    #print(type(data))
+    #print(data.keys())
+    # print(data.values())
+w = None
+w = data['weather']['temperature']
+print(w) # val1
+i = None
+s = None
+for i in data['sources']:
+    print(i['station_name'])
+    s = (i['station_name'])
+    print(s)
+    #break
+    continue
+    print(s)
